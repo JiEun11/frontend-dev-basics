0d62b22794436384bb57a29d8aef3882bb6dfbba
Bella <shdudtnr3939@naver.com>
2022-02-22 11:04:54 +0900

    JS practices #11

---
+++ javascript-practices/basics/ex17.js
@@ -0,0 +1,8 @@
+console.log("+++++++++++++ global object ++++++++++++++");
+
+global.name  = '둘리';
+console.log(name, global.name);
+
+// cf
+let email = 'dooly@gmail.com';
+console.log(email, global.email);
