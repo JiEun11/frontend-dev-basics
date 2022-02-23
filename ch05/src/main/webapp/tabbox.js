e6a6d9305a9d1f93375335c49e7a9de3859354d5
Bella <shdudtnr3939@naver.com>
2022-02-23 14:52:30 +0900

    22.02.23 DOM API #JS Modularization

---
+++ ch05/src/main/webapp/tabbox.js
@@ -0,0 +1,27 @@
+var tabBox = {
+	
+	_onWindowLoad: function(){
+		console.log('dom loaded!');
+		
+		var divTabBox = document.getElementsByClassName("tab-box")[0];
+		var ul = divTabBox.childNodes[1];
+		
+		var liTabs = ul.getElementsByTagName("li");
+		for(var i = 0; i < liTabs.length; i++){
+			liTabs[i].addEventListener("click",this.onTabClicked);
+		}
+	
+	},
+	_onTabClicked: function(){
+		// selected 를 가지고 있는 애를 찾아서 unselected 하기
+		var lisSelected = document.getElementsByClassName("selected");
+		(lisSelected.length === 1) && (lisSelected[0].className = "");
+
+		// select
+		this.className = 'selected';
+	},
+	
+	init: function() {
+		window.addEventListener('load', this.onWindowLoad.bind(this));
+	}
+}
