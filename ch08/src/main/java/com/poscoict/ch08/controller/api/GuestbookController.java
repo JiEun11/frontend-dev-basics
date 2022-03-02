4b5c91f7135969c2372b47e9844697c04726e891
Bella <shdudtnr3939@naver.com>
2022-03-02 15:29:40 +0900

    22.03.02 guestbook spa ajax - 2 (CRU)

--- ch08/src/main/java/com/poscoict/ch08/controller/api/GuestbookController.java
+++ ch08/src/main/java/com/poscoict/ch08/controller/api/GuestbookController.java
@@ -1,9 +1,13 @@
 package com.poscoict.ch08.controller.api;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.poscoict.ch08.controller.dto.JsonResult;
@@ -22,4 +26,36 @@ public class GuestbookController {
 		
 		return JsonResult.success(vo);
 	}
+	
+	@ResponseBody
+	@RequestMapping(value="/list", method=RequestMethod.GET) 
+	public JsonResult ex01(@RequestParam(value="sn", required=true,defaultValue="-1") Long no) {
+		// guestbookService.getMessageList(no); repository 하나 overloading, no=-1이면 where절 없음 
+		List<GuestbookVo> list = new ArrayList<>();
+		
+		GuestbookVo vo1 = new GuestbookVo();
+		vo1.setNo(3L);
+		vo1.setName("jieun3");
+		vo1.setMessage("Hello3");
+		list.add(vo1);
+		
+		GuestbookVo vo2 = new GuestbookVo();
+		vo2.setNo(2L);
+		vo2.setName("jieun2");
+		vo2.setMessage("Hello2");
+		list.add(vo2);
+		
+		GuestbookVo vo3 = new GuestbookVo();
+		vo3.setNo(1L);
+		vo3.setName("jieun1");
+		vo3.setMessage("Hello1");
+		list.add(vo3);
+		
+	
+		
+		return JsonResult.success(list);
+	}
+	
+	
+	
 }
--- ch08/src/main/java/com/poscoict/ch08/controller/test/GuestbookTestController.java
+++ ch08/src/main/java/com/poscoict/ch08/controller/test/GuestbookTestController.java
@@ -6,10 +6,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/test/guestbook")
 public class GuestbookTestController {
-	
-	// 1. add 
+
+	// 1. add
 	@RequestMapping("/ex01")
 	public String ex01() {
 		return "guestbook/ex01";
+	}
+
+	// 2. list
+	@RequestMapping("/ex02")
+	public String ex02() {
+		return "guestbook/ex02";
+	}
+
+	// 2. delete
+	@RequestMapping("/ex03")
+	public String ex03() {
+		return "guestbook/ex03";
 	}
 }
