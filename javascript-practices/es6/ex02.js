bc9da59f4b8d6132670b7399c84b79c7288633cd
Bella <shdudtnr3939@naver.com>
2022-03-11 11:29:53 +0900

    22.03.11 ES6 practices ~Arrow Function

---
+++ javascript-practices/es6/ex02.js
@@ -0,0 +1,22 @@
+/* 
+  const : Block Scope의 상수
+*/
+
+// 1. Block Scope 확인
+try {
+  if(true){
+    const NUM = 10;
+  }
+  console.log(NUM);
+} catch (error) {
+  console.log(error);
+}
+
+// 2. error: 대입(Assignment) 에러
+try {
+  const NUM = 10;
+  NUM = 20;
+  
+} catch (error) {
+  console.log(error);
+}
---
+++ javascript-practices/es6/ex05.js
@@ -0,0 +1,49 @@
+/* 
+  Arrow Function
+*/
+
+const power1 = function(x){
+  return x * x;
+}
+
+const numbers = [1, 2, 3, 4, 5];
+numbers.forEach(function(n){
+  process.stdout.write(`${n} : ${power1(n)}\t`);
+});
+
+
+// ex01
+console.log("\n---------------------\n");
+const power2 = x => x *x;
+numbers.forEach(function(n){
+  process.stdout.write(`${n} : ${power2(n)}\t`);
+});
+
+
+// ex02
+console.log("\n---------------------\n");
+numbers.forEach(function(n){
+  process.stdout.write(`${n} : ${(x => x *x)(n)}\t`);
+});
+
+
+// ex03 
+console.log("\n---------------------\n");
+numbers.forEach(n => process.stdout.write(`${n} : ${(x => x *x)(n)}\t`));
+
+
+// ex04 : this를 어휘적으로 바인딩 가능
+console.log("\n---------------------\n");
+const dooly = {
+  name: '둘리',
+  friends: ['또치', '마이콜', '도우너'],
+  printFriends: function() {
+    // this.friends.forEach(function(f){
+    //   console.log(`${this.name}의 친구 ${f}`);
+    // }.bind(this));
+    this.friends.forEach(f => console.log(`${this.name}의 친구 ${f}`));
+  }
+};
+
+dooly.printFriends();
+
