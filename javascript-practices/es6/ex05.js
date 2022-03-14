21664c9fbc799553b8cc7f09535e5e6c99a973c6
Bella <shdudtnr3939@naver.com>
2022-03-14 10:40:25 +0900

    22.03.14 arrow function practices #2

--- javascript-practices/es6/ex05.js
+++ javascript-practices/es6/ex05.js
@@ -6,6 +6,12 @@ const power1 = function(x){
   return x * x;
 }
 
+const p2 = (x) => {
+  return x * x;
+}
+
+const p3 = (x) => x*x ;
+
 const numbers = [1, 2, 3, 4, 5];
 numbers.forEach(function(n){
   process.stdout.write(`${n} : ${power1(n)}\t`);
